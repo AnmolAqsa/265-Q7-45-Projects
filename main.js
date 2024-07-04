590ed152b4ee4129bf8dcc0002a10295c808c38b
AnmolAqsa <165940283+AnmolAqsa@users.noreply.github.com>
2024-07-04 12:24:11 -0700

    Add files via upload
    Q7, 45 Projects

---
+++ main.js
@@ -0,0 +1,8 @@
+var number = 6 + 2;
+console.log(number);
+var number1 = 12 - 4;
+console.log(number1);
+var number2 = 2 * 4;
+console.log(number2);
+var number3 = 32 / 4;
+console.log(number3);
---
+++ main.ts
@@ -0,0 +1,16 @@
+/*
+7. Number Eight: Write addition, subtraction, multiplication, and division operations that each
+result in the number 8. Be sure to enclose your operations in print statements to see the results. 
+*/
+
+let number = 6+2;
+console.log(number);
+
+let number1 = 12-4;
+console.log(number1);
+
+let number2 = 2*4;
+console.log(number2);
+
+let number3 = 32/4;
+console.log(number3);
